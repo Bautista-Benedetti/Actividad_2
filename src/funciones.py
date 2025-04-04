9c22cfc4d113d57827295a4cdcec4f0b12dfc9b1
bautista benedetti <bb.bautistabenedetti@gmail.com>
2025-04-04 14:12:10 -0300

    separo el main para que quede mas limpio el programa

--- src/funciones.py
+++ src/funciones.py
@@ -1,34 +1,3 @@
-rounds = [
-{'Shadow': {'kills': 2, 'assists': 1, 'deaths': True},
-'Blaze': {'kills': 1, 'assists': 0, 'deaths': False},
-'Viper': {'kills': 1, 'assists': 2, 'deaths': True},
-'Frost': {'kills': 0, 'assists': 1, 'deaths': False},
-'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}},
-
-{'Shadow': {'kills': 0, 'assists': 2, 'deaths': False},
-'Blaze': {'kills': 2, 'assists': 0, 'deaths': True},
-'Viper': {'kills': 1, 'assists': 1, 'deaths': False},
-'Frost': {'kills': 2, 'assists': 1, 'deaths': True},
-'Reaper': {'kills': 0, 'assists': 1, 'deaths': False}},
-
-{'Shadow': {'kills': 1, 'assists': 0, 'deaths': False},
-'Blaze': {'kills': 2, 'assists': 2, 'deaths': True},
-'Viper': {'kills': 1, 'assists': 1, 'deaths': True},
-'Frost': {'kills': 0, 'assists': 1, 'deaths': False},
-'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}},
-
-{'Shadow': {'kills': 2, 'assists': 1, 'deaths': False},
-'Blaze': {'kills': 1, 'assists': 0, 'deaths': True},
-'Viper': {'kills': 0, 'assists': 2, 'deaths': False},
-'Frost': {'kills': 1, 'assists': 1, 'deaths': True},
-'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}},
-
-{'Shadow': {'kills': 1, 'assists': 2, 'deaths': True},
-'Blaze': {'kills': 0, 'assists': 1, 'deaths': False},
-'Viper': {'kills': 2, 'assists': 0, 'deaths': True},
-'Frost': {'kills': 1, 'assists': 1, 'deaths': False},
-'Reaper': {'kills': 1, 'assists': 1, 'deaths': True}}
-]
 
 def calcular_clasificacion_y_mvp(rounds):
     clasificaciones = []
@@ -74,8 +43,3 @@ def mostrar_ranking_final(jugadores_totales):
     for jugador, stats in sorted(jugadores_totales.items(), key=lambda x: x[1]['puntos'], reverse=True):
         print(f"{jugador:<10}{stats['kills']:<8}{stats['assists']:<13}{stats['deaths']:<9}{stats['mvps']:<7}{stats['puntos']:<6}")
     print("________________________________________________________")
-
-
-# Ejecutamos las funciones
-clasificaciones, jugadores_totales = calcular_clasificacion_y_mvp(rounds)
-mostrar_ranking_final(jugadores_totales)
